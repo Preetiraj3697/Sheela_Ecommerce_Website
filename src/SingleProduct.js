d4cc5e84c1c45c65bff6cabf788c027a83cc3b06
Preetiraj3697 <satendra11kum@gmail.com>
2022-10-12 19:29:09 +0530

    add day-20

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -83,6 +83,10 @@ const Wrapper = styled.section`
   .container {
     padding: 9rem 0;
   }
+  .product_images{
+    display:flex;
+    align-items:center;
+  }
   .product-data {
     display: flex;
     flex-direction: column;
--- src/components/MyImage.js
+++ src/components/MyImage.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+import styled from 'styled-components'
+const MyImage = ({imgs = [{url:""}]}) => {
+  const [mainImage , setMainImage] = useState(imgs[0]);
 
-const MyImage = () => {
+  console.log('imgs: ', imgs);
   return (
-    <div>
-      Image
+    <Wrapper>
+      <div className='grid grid-four-column'>
+       {
+        imgs.map((curElem,index)=>{
+          return(
+            <figure>
+              <img src={curElem.url} alt={curElem.filename} className="box-image-style"  key={index} onClick={()=> setMainImage(curElem)}/>
+            </figure>
+          )
+        })
+       }
+      </div>
+    {/* 2 column */}
+    <div className='main-screen'>
+      <img src={mainImage.url} alt={mainImage.filename} />
     </div>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.section`
+display:grid;
+grid-template-columns:0.4fr 1fr;
+gap:1rem;
+img{
+  width:100px;
+}
+`
 export default MyImage
