97756941a965c2c61021786b00192f7fe3c45044
Preetiraj3697 <satendra11kum@gmail.com>
2022-10-13 16:31:33 +0530

    add

--- src/components/MyImage.js
+++ src/components/MyImage.js
@@ -27,11 +27,48 @@ const MyImage = ({imgs = [{url:""}]}) => {
 }
 
 const Wrapper = styled.section`
-display:grid;
-grid-template-columns:0.4fr 1fr;
-gap:1rem;
-img{
-  width:100px;
-}
-`
-export default MyImage
+  display: grid;
+  grid-template-columns: 0.4fr 1fr;
+  gap: 1rem;
+  .grid {
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+    width: 100%;
+    gap: 1rem;
+    /* order: 2; */
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      background-size: cover;
+      object-fit: contain;
+      cursor: pointer;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+  .main-screen {
+    display: grid;
+    place-items: center;
+    order: 1;
+    img {
+      max-width: 100%;
+      height: auto;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+  .grid-four-column {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 1fr);
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    display: flex;
+    flex-direction: column;
+    order: 1;
+    .grid-four-column {
+      grid-template-rows: 1fr;
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+`;
+
+export default MyImage;
